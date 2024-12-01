86658ad09c4a0526fc2ab85c10c525ef6584fee9
Hidayat Rzayev <rzayev.hidayat@gmail.com>
2024-12-01 15:06:52 +0100

    Simplify solution to Day_01

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,11 +3,11 @@ import kotlin.math.abs
 fun main() {
     fun part1(input: List<String>): Int = input.buildLists().let { (leftIds, rightIds) ->
         leftIds.sorted().zip(rightIds.sorted())
-            .fold(initial = 0) { totalDistance, (leftId, rightId) -> totalDistance + abs(leftId - rightId) }
+            .sumOf { (leftId, rightId) -> abs(leftId - rightId) }
     }
 
     fun part2(input: List<String>): Int = input.buildLists().let { (leftIds, rightIds) ->
-        leftIds.fold(initial = 0) { similarityScore, id -> similarityScore + id * rightIds.count { it == id } }
+        leftIds.sumOf { id -> id * rightIds.count { it == id } }
     }
 
     val input = readInput("Day01")
@@ -15,15 +15,8 @@ fun main() {
     part2(input).println()
 }
 
-private fun List<String>.buildLists(): Pair<List<Int>, List<Int>> {
-    val leftIds = mutableListOf<Int>()
-    val rightIds = mutableListOf<Int>()
-    forEach { line ->
-        with(line.split(Regex("\\s+"))) {
-            leftIds.add(first().toInt())
-            rightIds.add(last().toInt())
-        }
+private fun List<String>.buildLists(): Pair<List<Int>, List<Int>> = map { line ->
+    with(line.split(Regex("\\s+"))) {
+        first().toInt() to last().toInt()
     }
-
-    return leftIds to rightIds
-}
+}.unzip()
