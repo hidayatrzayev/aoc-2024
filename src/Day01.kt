27681a0beea6fcaa0ea385d89d3792c35a1b8a63
Hidayat Rzayev <rzayev.hidayat@gmail.com>
2024-12-01 10:47:12 +0100

    Solutions to Day_01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,29 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Int = input.buildLists().let { (leftIds, rightIds) ->
+        leftIds.sorted().zip(rightIds.sorted())
+            .fold(initial = 0) { totalDistance, (leftId, rightId) -> totalDistance + abs(leftId - rightId) }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Int = input.buildLists().let { (leftIds, rightIds) ->
+        leftIds.fold(initial = 0) { similarityScore, id -> similarityScore + id * rightIds.count { it == id } }
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun List<String>.buildLists(): Pair<List<Int>, List<Int>> {
+    val leftIds = mutableListOf<Int>()
+    val rightIds = mutableListOf<Int>()
+    forEach { line ->
+        with(line.split(Regex("\\s+"))) {
+            leftIds.add(first().toInt())
+            rightIds.add(last().toInt())
+        }
+    }
+
+    return leftIds to rightIds
 }
